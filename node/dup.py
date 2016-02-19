d907e739257c9d096c61478026235dfef93e6eff
Simon Beal <simon.d.beal@gmail.com>
2016-02-19 14:36:36 +0000

    Add autoassign_var Duplicate now takes a perm integer argument for how many times to duplicate

--- node/dup.py
+++ node/dup.py
@@ -5,6 +5,26 @@ from nodes import Node
 class Duplicate(Node):
     char = "D"
     args = 1
-    results = 2
+    results = 0
+    
+    def __init__(self, amount):
+        self.results = amount
+        
     def func(self, a):
-        return [a,a]
+        return [a]*self.results
+    
+    def __repr__(self):
+        return "%s: %d"%(self.__class__.__name__, self.results)
+        
+    @classmethod
+    def accepts(cls, code):
+        if code[0] == cls.char:
+            new_code, digits = NumericLiteral.accepts(code[1:])
+            if new_code is None:
+                digits = 2
+                code = code[1:]
+            else:
+                digits = digits([])[0]
+                code = new_code
+            return code, cls(digits)
+        return None, None
