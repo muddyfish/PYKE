715ff30c910ffc74891b612bf29cefbab3712d49
Simon Beal <simon.d.beal@gmail.com>
2016-02-19 14:36:36 +0000

    Add autoassign_var Duplicate now takes a perm integer argument for how many times to duplicate

---
+++ node/autoassign_var.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+from nodes import Node
+
+import copy
+import settings
+
+class AutoAssignVar(Node):
+    char = "i"
+    args = 0
+    results = 1
+    
+    def prepare(self, stack):
+        if hasattr(self.__class__, "contents"):
+            self.func = self.retrieve
+        else:
+            self.args = 1
+            self.func = self.store
+            
+    def retrieve(self):
+        return copy.deepcopy(self.__class__.contents)
+    
+    def store(self, arg):
+        self.__class__.contents = arg
+        if settings.WARNINGS: print "Stored %r in i"%arg
+        return arg
