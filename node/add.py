af94e24506d77afa9970978d04c6dbc8547cf3f0
unknown <simon@catbus.co.uk>
2016-02-20 19:36:17 +0000

    Add addition between listt:s

--- node/add.py
+++ node/add.py
@@ -9,5 +9,15 @@ class Add(Node):
     char = "+"
     args = 2
     results = 1
+    
+    def join_sequence(self, a:Node.sequence, b:Node.sequence):
+        return[type(a)(list(a)+list(b))]
+    
+    def append_sequence(self, a:Node.sequence, b):
+        return[type(a)(list(a)+[b])]
+    
+    def prepend_sequence(self, a, b:Node.sequence):
+        return[type(b)([a]+list(b))]
+    
     def func(self, a,b):
         return a+b
