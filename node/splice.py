82effb647ae63f1e32a12fc66f37ab53f31de7f6
unknown <simon@catbus.co.uk>
2016-02-20 16:45:00 +0000

    Add priority system Functions with a higher priority get ran Update nodes to use new system Add functions to splice

--- node/splice.py
+++ node/splice.py
@@ -6,5 +6,19 @@ class Splice(Node):
     char = ":"
     args = 3
     results = 1
-    def func(self, a,b,c):
+    
+    def splice(self, a:Node.indexable, b:int, c:int):
         return a[b:c]
+    
+    def range(self, a:int, b:int, c:int):
+        return [list(range(a,b,c))]
+    
+    def multi_assign(self, a:Node.indexable, b: Node.indexable, c: Node.indexable):
+        for i in b:
+            a[i] = c[i]
+        return [a]
+    
+    def multi_assign_generic(self, a:Node.indexable, b: Node.indexable, c):
+        for i in b:
+            a[i] = c
+        return [a]
--- node/tail.py
+++ node/tail.py
@@ -6,6 +6,9 @@ class Tail(Node):
     char = "t"
     args = 1
     results = 1
-    def func(self, inp):
-        if isinstance(inp, int): return inp-1
+    
+    def sub_one(self, inp: Node.number):
+        return inp-1
+    
+    def first(self, inp: Node.indexable):
         return inp[1:]
