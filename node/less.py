9a411ef5d3d15c8149b40b537f5d6e9cea3b16ec
Simon Beal <simon.d.beal@gmail.com>
2016-03-14 18:01:45 +0000

    Add first n and last n

--- node/less.py
+++ node/less.py
@@ -8,3 +8,10 @@ class Equals(Node):
     results = 1
     def func(self, a,b):
         return a<b
+    
+    def indexable_1(self, a:Node.indexable, b:int):
+        return a[:b]
+    
+    def indexable_2(self, a:int, b:Node.indexable):
+        return b[:len(b)-a]
+    
