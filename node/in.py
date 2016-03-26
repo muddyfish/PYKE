bbfdf968c7481a5818c48156100bf1ad97c70427
Simon Beal <simon.d.beal@gmail.com>
2016-03-26 15:29:14 +0000

    Add set_bit and unset_bit nodes Fix count Fix index Add more tests!

--- node/in.py
+++ node/in.py
@@ -2,10 +2,26 @@
 
 from nodes import Node
 
+#Unset bit
+@Node.test("{", [1,1], [1])
+@Node.test("{", [1,3], [1])
+@Node.test("{", [2,5], [1])
+@Node.test("{", [0,7], [6])
+#In
+@Node.test("{", [[6,2,1],6], [1])
+@Node.test("{", [[6,2,1],3], [0])
 class In(Node):
     char = "{"
     args = 2
     results = 1
     def func(self, a,b:Node.indexable):
-        """a in b"""
-        return a in b
+        """a in b. returns an int"""
+        return (a in b) + 0
+    
+    def unset_bit(self, a: int, b: int):
+        """Unset bit b in a"""
+        bit = 2**b
+        if a&bit:
+            return a^bit
+        return a
+    
