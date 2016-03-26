50bf03ff9bfe51cf31503bb8b00e3bce330d73b4
Simon Beal <simon.d.beal@gmail.com>
2016-03-26 16:26:58 +0000

    Bugfixes Add more tests

--- node/index.py
+++ node/index.py
@@ -2,6 +2,15 @@
 
 from nodes import Node
 
+#At
+@Node.test("@", [1,"tes"], ["e"])
+@Node.test("@", [-1,[1,2,3]], [3])
+#Index
+@Node.test("@", [[1,2,3],1], [0])
+@Node.test("@", [[1,2,3],3], [2])
+#Set_bit
+@Node.test("@", [0, 2], [3])
+@Node.test("@", [2, 1], [5])
 class Index(Node):
     char = "@"
     args = 2
@@ -11,9 +20,9 @@ class Index(Node):
         """a[b]"""
         return a[b]
     
-    def index(self, a: Node.indexable, b):
-        """a.index(b)"""
-        return a.index(b)
+    def index(self, a, b: Node.indexable):
+        """b.index(a)"""
+        return b.index(a)
     
     def set_bit(self, a: int, b: int):
         """Set bit b in a"""
