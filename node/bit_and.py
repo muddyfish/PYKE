4a416acb21d7b90f7a95f05556e03131e244053d
unknown <simon@catbus.co.uk>
2016-03-31 15:50:59 +0100

    Add deep_apply, applies a node recursively on an nD tree Add nd_range, creates a nd array based on a sequence of dimension sizes Add bitwise functions  - and  - or  - xor

---
+++ node/bit_and.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from nodes import Node
+
+class BitAnd(Node):
+    char = ".&"
+    args = 2
+    results = 1
+    
+    @Node.test_func([4,5], [4])
+    def func(self, a,b):
+        """a&b"""
+        return a&b
---
+++ node/bit_or.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from nodes import Node
+
+class BitOr(Node):
+    char = ".|"
+    args = 2
+    results = 1
+    
+    @Node.test_func([4,1], [5])
+    def func(self, a,b):
+        """a|b"""
+        return a|b
