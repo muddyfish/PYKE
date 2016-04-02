c2a0a52dbf8b35fea2281fcb68e2cb89827bd540
Simon Beal <simon.d.beal@gmail.com>
2016-04-02 18:46:19 +0100

    Add deep for apply node, takes an ast instead of a node Fix end node Fix index node

--- node/deep_for_apply.py
+++ node/deep_for_apply.py
@@ -2,13 +2,13 @@ import lang_ast
 from nodes import Node
 import copy
 
-class DeepApply(Node):
-    char = "A"
+class DeepForApply(Node):
+    char = "a"
     args = 1
     results = 1
     
-    def __init__(self, node):
-        self.node = node
+    def __init__(self, ast: Node.EvalLiteral):
+        self.ast = ast
     
     @Node.test_func([[[(0, 0), (0, 1)], [(1, 0), (1, 1)]]], [[[0, 1], [1, 2]]], "+")
     def func(self, seq: Node.sequence):
@@ -22,20 +22,9 @@ class DeepApply(Node):
             rtn = []
             for i in copy.deepcopy(seq):
                 try:
-                    val = self.node(i)
+                    val = self.ast.run(list(i))
                 except AssertionError:
-                    val = self.node([i])
+                    val = self.ast.run([list(i)])
                 if len(val) > 1: rtn.append(val)
                 else: rtn.extend(val)
             return rtn
-    
-    def __repr__(self):
-        return "%s: %r"%(self.__class__.__name__, self.args)
-        
-    @classmethod
-    def accepts(cls, code):
-        if code[0] == cls.char:
-            code, node = lang_ast.AST.add_node(code[1:])
-            assert(node is not None)
-            return code, cls(node)
-        return None, None
