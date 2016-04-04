8e2ac180fd786b7627ce26ed09048732dd7a68a4
Simon Beal <simon.d.beal@gmail.com>
2016-04-04 13:32:30 +0100

    Transpose returns a list of strs if input was so Reverse order for args for index - BREAKS STUFF PrintGrid doesn't pad if all strs len 1

--- node/transpose.py
+++ node/transpose.py
@@ -10,4 +10,6 @@ class Transpose(Node):
     @Node.test_func([[[1,2,3],[4,5,6]]], [[[1,4],[2,5],[3,6]]])
     def transpose(self, inp: Node.sequence):
         """zip(*inp)"""
+        if all(isinstance(i,str)for i in inp):
+            return[["".join(s)for s in zip(*inp)]]
         return[list(map(list,zip(*inp)))]
