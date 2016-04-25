e5ccec0f667e7f84f3e2812455ddcd5f947f21d6
Simon Beal <simon.d.beal@gmail.com>
2016-04-25 19:26:13 +0100

    Remove debugging

--- node/iterator.py
+++ node/iterator.py
@@ -11,6 +11,5 @@ class Iterator(Node):
     def func(self):
         """contents += 1
 return contents-1"""
-        print(Iterator.contents)
         Iterator.contents += 1
         return Iterator.contents-1
