1285519c34ac2299b565aa8e2290a436964f7f66
unknown <simon@catbus.co.uk>
2016-05-08 13:13:39 +0100

    Add if_kill node where kills stack if false

---
+++ node/if_kill.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import lang_ast
+from nodes import Node
+
+class IfKill(Node):
+    char = ".I"
+    args = 1
+    results = None
+    
+    def __init__(self, ast: Node.EvalLiteral):
+        self.ast = ast
+        if self.ast.nodes == []:
+            self.ast.add_node("\n")
+        
+    def prepare(self, stack):
+        self.args = max(len(stack), 1)
+            
+    @Node.test_func([5,1], [10], "}")
+    @Node.test_func([5,0], [], "}")
+    def func(self, *args):
+        """Takes stack.
+if arg1: stack = eval_literal(stack[:-1]) (extend mode)
+else: stack = []"""
+        args = list(args)
+        if args[-1]:
+            args = args[:-1]
+            stack = self.ast.run(args)
+            return stack
+        return []
+    
+    def __repr__(self):
+        return "%s: %r"%(self.__class__.__name__, self.ast)
+        
