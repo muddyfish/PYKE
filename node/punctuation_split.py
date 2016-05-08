60d168f57e35f014044360ee9366f6933113f1bf
unknown <simon@catbus.co.uk>
2016-05-08 13:39:17 +0100

    Add split by punctuation

---
+++ node/punctuation_split.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from nodes import Node
+
+class PuncSplit(Node):
+    char = ".c"
+    args = 1
+    results = 1
+    
+    @Node.test_func(["I went to the shop."], [["I", "went", "to", "the", "shop"]])
+    @Node.test_func(["bob-bob is sad."], [["bob-bob", "is", "sad"]])
+    @Node.test_func(["Why?"], [["Why"]])
+    @Node.test_func(["He, She, It."], [["He", "She", "It"]])
+    def split(self, inp:str):
+        """Split by space and remove punctuation"""
+        inp = inp.split()
+        rtn = []
+        for i in inp:
+            if i[-1] in ".,;'\"!?\\/])}":
+                i = i[:-1]
+            if i[0] in "[({":
+                i = i[1:]
+            rtn.append(i)
+        return [rtn]
+        
