1f9aa1611fc69655e2f1491273e795705b83bc29
unknown <simon@catbus.co.uk>
2016-05-14 20:05:31 +0100

    Add  mixed base node. Deep_apply now goes for deep_apply when no stack rather than splat

--- node/pow.py
+++ node/pow.py
@@ -24,3 +24,17 @@ class Pow(Node):
         """Does string end with suffix?"""
         return int(string.endswith(suffix))
     
+    @Node.test_func([[1, 2, 3, 4, 5], [10, 10, 4, 62, 7]], [22167])
+    @Node.test_func([[4, 2], [5, 10]], [42])
+    @Node.test_func([[3, 0, 7], [5, 1, 10]], [37])
+    def mixed_base(self, inp:Node.sequence, bases:Node.sequence):
+        """Convert from a mixed base number to an integer"""
+        prod = 1
+        for val in bases:
+            prod *= val
+        cur = 0
+        for e, i in enumerate(inp):
+            prod //= bases[e]
+            cur += i*prod
+        return cur
+        
