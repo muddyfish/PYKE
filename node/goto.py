4b2fffcffd232547566eeb74d5eb49c1d3a1351e
unknown <simon@catbus.co.uk>
2016-05-25 18:12:29 +0100

    Goto now acts like return in function Add goto to docs Bugfix repeat

--- node/goto.py
+++ node/goto.py
@@ -5,7 +5,17 @@ import lang_ast
 
 class Goto(Node):
     char = "r"
+    args = 0
     
     def prepare(self, stack):
-        """Goto the start of the program, keeping the same stack"""
         raise lang_ast.GotoStart(stack)
+    
+    @Node.is_func
+    def goto_start(self):
+        """Goto the start of the program, keeping the same stack"""
+        pass
+    
+    @Node.is_func
+    def func_return(self):
+        """If in a function, return the current stack"""
+        pass
