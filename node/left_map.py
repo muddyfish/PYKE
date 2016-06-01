a7808d756c53ae0e7522bd350213979b31821c6e
unknown <simon@catbus.co.uk>
2016-06-01 13:53:20 +0100

    Map now doesn't reverse stack

--- node/left_map.py
+++ node/left_map.py
@@ -7,8 +7,12 @@ class LeftMap(Map):
     contents = -1
     
     def prepare(self, stack):
+        if len(stack) == 0:
+            self.add_arg(stack)
         if isinstance(stack[0], dict):
             self.args = max(1, self.args-2)
+        old_call = self.node.__call__
+        self.node.__call__ = lambda *args:old_call(*reversed(args))
     
 for func in LeftMap.get_functions():
     if hasattr(func, "tests"):
