0e3dc24b02165657c670acc8c2064e6a6cdc9dc9
unknown <simon@catbus.co.uk>
2016-06-06 21:51:45 +0100

    Update sort_eval to fix multiple args

--- node/sort.py
+++ node/sort.py
@@ -18,9 +18,11 @@ class Sort(Node):
         return [Sort.sort_list(a)]
     
     @staticmethod
-    def sort_list(a):
+    def sort_list(a, deep_sort = True):
         if isinstance(a, (str, float, int, complex)):return a
-        return sorted(a, key = lambda x:(str(type(x)), Sort.sort_list(x)))
+        key = [lambda x:(str(type(x)), x),
+               lambda x:(str(type(x)), Sort.sort_list(x))][deep_sort]
+        return sorted(a, key = key)
     
     @Node.test_func([3], [[1,2,3]])
     def one_range(self, a:int):
