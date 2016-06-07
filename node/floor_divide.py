a0aa74d9e6295e34f02b4eefd76e7eb9a1e6425f
unknown <simon@catbus.co.uk>
2016-06-07 10:27:17 +0100

    Add a group chunk, chunks a list into N groups

--- node/floor_divide.py
+++ node/floor_divide.py
@@ -22,3 +22,26 @@ If the separator is not found,
 return a 3-list containing the string itself,
 followed by two empty strings."""
         return [list(string.partition(sep))]
+    
+    @Node.test_func(["134", 1], [["134"]])
+    @Node.test_func(["1234", 2], [["12", "34"]])
+    @Node.test_func(["1234", 3], [["1", "2", "34"]])
+    @Node.test_func([[4,8,15,16,23,42], 5], [[[4],[8],[15],[16],[23,42]]])
+    def chunk(self, inp:Node.indexable, num:Node.number):
+        """Return inp seperated into num groups"""
+        rtn = []
+        last = 0
+        size = len(inp)//num
+        for i in range(size, len(inp), size):
+            rtn.append(inp[last:i])
+            last = i
+        if len(rtn) != num:
+            rtn.append(inp[last:])
+        else:
+            rtn[-1] += inp[last:]
+        if len(rtn):
+            if isinstance(inp, str):
+                rtn[-1] = "".join(rtn[-1])
+            else:
+                rtn[-1] = type(inp)(rtn[-1])
+        return [rtn]
