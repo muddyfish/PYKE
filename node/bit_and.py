059f0934cb023cf7b73572403d1a8ff1765b1633
Simon Beal <simon.d.beal@gmail.com>
2016-06-23 21:30:35 +0100

    Add setwise and or and xor

--- node/bit_and.py
+++ node/bit_and.py
@@ -7,7 +7,22 @@ class BitAnd(Node):
     args = 2
     results = 1
     
+    def prepare(self, stack):
+        if len(stack) == 0:
+            self.add_arg(stack)
+        if isinstance(stack[0], Node.sequence):
+                self.args = 1
+            
     @Node.test_func([4,5], [4])
     def func(self, a:int,b:int):
         """a&b"""
         return a&b
+
+    def and_seq(self, seq:Node.sequence):
+        """Setwise and of sequences"""
+        if len(seq)==0: return seq
+        rtn = []
+        for i in seq[0]:
+            if False not in [i in sub for sub in seq]:
+                rtn.append(i)
+        return [rtn]
