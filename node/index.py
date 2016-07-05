98f0079d7cb3d9e837dc1b36e9f7b4f5cd051f43
unknown <simon@catbus.co.uk>
2016-07-05 20:14:31 +0100

    BREAKING CHANGE - Swap index and at Fix index when not in list

--- node/index.py
+++ node/index.py
@@ -7,11 +7,11 @@ class Index(Node):
     args = 2
     results = 1
     
-    @Node.test_func([1,"tes"], ["e"])
-    @Node.test_func([-1,[1,2,3]], [3])
-    def at(self, a:int, b: Node.indexable):
-        """b[a]"""
-        return[b[a]]
+    @Node.test_func(["tes",1], ["e"])
+    @Node.test_func([[1,2,3],-1], [3])
+    def at(self, a: Node.indexable, b:int):
+        """a[b]"""
+        return[a[b]]
     
     
     @Node.test_func([2, {1:2,2:3}], [3])
@@ -20,16 +20,20 @@ class Index(Node):
     def dict_at(self, a, b:dict):
         return[b[a]]
     
-    @Node.test_func([[1,2,3],1], [0])
-    @Node.test_func([[1,2,3],3], [2])
-    @Node.test_func(["hello","e"], [1])
-    @Node.test_func(["hello","?"], [-1])
+    @Node.test_func([1, [1,2,3]], [0])
+    @Node.test_func([3, [1,2,3]], [2])
+    @Node.test_func([4, [1,2,3]], [-1])
+    @Node.test_func(["e", "hello"], [1])
+    @Node.test_func(["?", "hello"], [-1])
     @Node.prefer
-    def index(self, a: Node.indexable, b):
-        """a.index(b)"""
-        if isinstance(a, str):
-            return a.find(b)
-        return a.index(b)
+    def index(self, a, b: Node.indexable):
+        """b.index(a)"""
+        if isinstance(b, str):
+            return b.find(a)
+        try:
+            return b.index(a)
+        except ValueError:
+            return -1
     
     @Node.test_func([2, 0], [3])
     @Node.test_func([1, 2], [5])
