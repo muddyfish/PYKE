b51bdccb58f08ac646f151f780fdbaa412be30ca
Simon Beal <simon.d.beal@gmail.com>
2016-10-07 14:12:18 +0100

    Add multi_var

---
+++ node/multi_var.py
@@ -0,0 +1,25 @@
+
+from nodes import Node
+
+class MultiVar(Node):
+    char = "'"
+    args = 0
+    results = None
+    contents = -1
+    
+    def __init__(self, node_1: Node.NodeSingle, node_2: Node.NodeSingle):
+        self.node_1 = node_1
+        self.node_2 = node_2
+        self.args = max([node_1.args, node_2.args])
+        
+    def prepare(self, stack):
+        if len(stack) == 0:
+            self.add_arg(stack)
+
+    @Node.is_func
+    def apply(self, *stack):
+        self.node_2.prepare(stack)
+        rtn = self.node_2(stack)
+        self.node_1.prepare(stack)
+        rtn.extend(self.node_1(stack))
+        return rtn
