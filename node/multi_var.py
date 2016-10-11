46f2b508eb5f4350a06bdf832c7481ad8d8aab33
pyke <pyke@pyke.catbus.co.uk>
2016-10-11 16:58:19 +0000

    Fix multivar for nodes with variable length stacks

--- node/multi_var.py
+++ node/multi_var.py
@@ -10,16 +10,14 @@ class MultiVar(Node):
     def __init__(self, node_1: Node.NodeSingle, node_2: Node.NodeSingle):
         self.node_1 = node_1
         self.node_2 = node_2
-        self.args = max([node_1.args, node_2.args])
         
     def prepare(self, stack):
-        if len(stack) == 0:
-            self.add_arg(stack)
+        self.node_1.prepare(stack)
+        self.node_2.prepare(stack)
+        self.args = max([self.node_1.args,self.node_2.args])
 
     @Node.is_func
     def apply(self, *stack):
-        self.node_2.prepare(stack)
         rtn = self.node_2(stack[:self.node_2.args])
-        self.node_1.prepare(stack)
         rtn.extend(self.node_1(stack[:self.node_1.args]))
         return rtn
