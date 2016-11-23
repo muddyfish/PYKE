e1cc669f64b983524bb4a2b6bc57ec341fd383f9
Simon Beal <simon.d.beal@gmail.com>
2016-11-23 17:53:28 +0000

    Translate now has default

--- node/translate.py
+++ node/translate.py
@@ -2,13 +2,18 @@
 
 from nodes import Node
 
+
 class Translate(Node):
     char = ".:"
     args = 3
     results = 1
     
-    def translate(self, inp:str, find:Node.indexable, replace:Node.indexable):
+    def translate(self, inp: str, find: Node.indexable, replace: Node.indexable):
         """Multiple find and replace"""
-        for i in zip(find,replace):
+        if len(replace) == 1:
+            for i in find:
+                inp = inp.replace(i, replace)
+            return inp
+        for i in zip(find, replace):
             inp = inp.replace(*i)
         return inp
