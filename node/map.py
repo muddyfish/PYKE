d55972b81094e5c74a605ed6879dcdadc06126ff
Simon Beal <simon.d.beal@gmail.com>
2016-11-25 13:20:40 +0000

    Pretify map

--- node/map.py
+++ node/map.py
@@ -1,6 +1,7 @@
 
 from nodes import Node
 
+
 class Map(Node):
     char = "m"
     args = 0
@@ -16,39 +17,36 @@ class Map(Node):
             self.add_arg(stack)
         if isinstance(stack[0], dict):
             self.args = max(1, self.args-2)
-        #else:
-        #    stack.reverse()
-        
-    #@Node.test_func([[1,2,3,4], 2.0], [[1,4,9,16]], "^")
     
-    @Node.test_func([[-4,2,3,4], 5], [[1,7,8,9]], "+")
-    @Node.test_func([[1,2,3,4], 2], [[2,4,8,16]], "^")
-    @Node.test_func([[1,0,"","hi"]], [[0,1,1,0]], "!")
-    def seq_map(self, seq:Node.sequence, *args):
+    @Node.test_func([[-4, 2, 3, 4], 5], [[1, 7, 8, 9]], "+")
+    @Node.test_func([[1, 2, 3, 4], 2], [[2, 4, 8, 16]], "^")
+    @Node.test_func([[1, 0, "", "hi"]], [[0, 1, 1, 0]], "!")
+    def seq_map(self, seq: Node.sequence, *args):
         end = []
         for i in seq:
             stack = [i, *args]
             self.node.prepare(stack)
             rtn = self.node(stack)
-            if len(rtn) == 1: rtn = rtn[0]
+            if len(rtn) == 1:
+                rtn = rtn[0]
             end.append(rtn)
         return [type(seq)(end)]
     
-    @Node.test_func([5, 2], [[2,3,4,5,6]], "+")
+    @Node.test_func([5, 2], [[2, 3, 4, 5, 6]], "+")
     def int_map(self, num:int, *args):
         seq = list(range(num))
         return self.seq_map(seq, *args)
 
     @Node.test_func(["seed", " "], [" s e e d"], "+")
-    def str_map(self, s:str, *args):
+    def str_map(self, s: str, *args):
         rtn = self.seq_map(list(s), *args)[0]
         try:
             return ["".join(rtn)]
         except TypeError:
             return [rtn]
     
-    @Node.test_func([{1:3,2:3,3:4}], [{1:3,2:6,3:12}], "*")
-    @Node.test_func([{1:3,2:3,3:4}], [{1:4,2:4,3:5}], "h")
+    @Node.test_func([{1: 3, 2: 3, 3: 4}], [{1: 3, 2: 6, 3: 12}], "*")
+    @Node.test_func([{1: 3, 2: 3, 3: 4}], [{1: 4, 2: 4, 3: 5}], "h")
     def dict_map(self, dic:dict, *args):
         rtn = {}
         for key in dic:
