f52ffb3b32ed7ba3fcc2408c15a581374cc07826
Simon Beal <simon.d.beal@gmail.com>
2016-12-26 14:52:43 +0000

    Add get_day_of_year

--- node/dict.py
+++ node/dict.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
+import datetime
+
 from nodes import Node
+
 
 class Dict(Node):
     char = "Y"
@@ -9,9 +12,13 @@ class Dict(Node):
     contents = 256
         
     @Node.test_func([[["test", 2], [2,3]]], [{"test":2,2:3}])
-    def func(self, a:Node.sequence):
+    def func(self, a: Node.sequence):
         """Turn a sequence into a dict"""
         return dict(a)
 
     def digits(self, num: int):
         return [[int(i) for i in str(num)]]
+
+    def get_day_of_year(self, time: Node.clock):
+        new_time = datetime.datetime(*time.time_obj[:7])
+        return new_time.timetuple().tm_yday
