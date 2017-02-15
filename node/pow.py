bb88fbfeabb49e576f1b9a7460b6bb7ecc4a1b02
Simon Beal <simon.d.beal@gmail.com>
2017-02-15 17:49:47 +0000

    Add `closest_to`, returns closest number in a list to a number Add more documentation to things in `pow`.

--- node/pow.py
+++ node/pow.py
@@ -25,14 +25,14 @@ class Pow(Node):
     @Node.test_func(["Test.", "."], [1])
     @Node.test_func(["Test.", "?"], [0])
     @Node.test_func(["Test.", "t."], [1])
-    def endswith(self, string:str, suffix:str):
+    def endswith(self, string: str, suffix: str):
         """Does string end with suffix?"""
         return int(string.endswith(suffix))
     
     @Node.test_func([[1, 2, 3, 4, 5], [10, 10, 4, 62, 7]], [22167])
     @Node.test_func([[4, 2], [5, 10]], [42])
     @Node.test_func([[3, 0, 7], [5, 1, 10]], [37])
-    def mixed_base(self, inp:Node.sequence, bases:Node.sequence):
+    def mixed_base(self, inp: Node.sequence, bases: Node.sequence):
         """Convert from a mixed base number to an integer"""
         prod = 1
         for val in bases:
@@ -43,11 +43,23 @@ class Pow(Node):
             cur += i*prod
         return cur
 
+    @Node.test_func(["abc", 5], ["abc  "])
+    @Node.test_func(["abc", 2], ["abc"])
+    @Node.test_func(["te  ", 6], ["te    "])
     def lpad(self, string: str, amount: int):
+        """Return string padded with spaces to make it `amount` in length"""
         return string.ljust(amount)
 
     def get_distance_to_sun(self, time: Node.clock, object: str):
-        """Gets the distance to the sun from `object`"""
+        """Gets the distance to the sun from `object` at `time`"""
         new_time = datetime.datetime(*time.time_obj[:7])
 
         return getattr(ephem, object)(new_time).sun_distance
+
+    @Node.test_func([[1, 2, 6, 7], 5], [6])
+    @Node.test_func([[-1, 0, 1], 0], [0])
+    @Node.test_func([[-1, 1], 0], [-1])
+    def closest_to(self, lst: Node.sequence, value: Node.number):
+        """Given a list of numbers, return the closest one to `value`.
+If 2 numbers are of the same distance, the one closest to the start of the original list is returned"""
+        return sorted(lst, key=lambda i: abs(i-value))[0]
